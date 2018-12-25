4d8ddc820af39d4569da7757820cb33d20ec163f
doni <doni@192.168.219.107>
2018-12-25 19:36:08 +0900

    added requestmapping

--- src/main/java/com/doni/springboot/examples/controller/HelloController.java
+++ src/main/java/com/doni/springboot/examples/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.doni.springboot.examples.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +13,7 @@ public class HelloController {
 	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 	
 	@GetMapping("/hello")
+//	@RequestMapping(value="/hello", method=RequestMethod.GET)
 	public String hello() {
 		logger.debug("debug Hello");
 		logger.info("info Hello");
