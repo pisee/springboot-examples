02f7d16fd183b5ffe7b6c2794e8e756c8096d494
doni <ds0703.lee@samsung.com>
2019-01-23 09:44:14 +0900

    get urlmappinghandlerinfo

--- src/test/java/com/doni/springboot/examples/SpringbootExamplesApplicationTests.java
+++ src/test/java/com/doni/springboot/examples/SpringbootExamplesApplicationTests.java
@@ -1,16 +1,50 @@
 package com.doni.springboot.examples;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.BeanFactoryUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.mvc.condition.ParamsRequestCondition;
+import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
+import org.springframework.web.servlet.mvc.condition.RequestMethodsRequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootExamplesApplicationTests {
 
+	@Resource
+	ApplicationContext context;
+	
+	@Autowired
+	private RequestMappingHandlerMapping requestMappingHandlerMapping;
+
+	
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void getHandlerMappingInfo() {
+		Map<RequestMappingInfo, HandlerMethod> map = requestMappingHandlerMapping.getHandlerMethods();
+		StringBuilder sb = new StringBuilder();
+		for (RequestMappingInfo keys : map.keySet()) {
+			sb.append(map.get(keys).getShortLogMessage()).append(" ").append(keys.getPatternsCondition()).append(" ")
+					.append(keys.getMethodsCondition()).append("\n");
+			
+		}
+		System.out.println(sb.toString());
+	}
 }
